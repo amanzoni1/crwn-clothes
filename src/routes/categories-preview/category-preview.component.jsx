428dffd10b1e3e12fd8f83e671bcd29a5a65de7a
Andrea Manzoni <a.manzoni1@proton.me>
2022-09-06 23:29:48 +0200

    new shop routes

---
+++ src/routes/categories-preview/category-preview.component.jsx
@@ -0,0 +1,26 @@
+import { useContext, Fragment } from "react";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+import './category-preview.style.scss';
+
+const CategoriesPreview = () => {
+
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <div className="Category-preview-container">
+      {
+        Object.keys(categoriesMap).map(title => { 
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })}
+    </div>
+  );
+};
+
+
+export default CategoriesPreview;
